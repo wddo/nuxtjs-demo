ade24bb5627b8bd22720c20c67ce8834368585eb
wddo <wddoddo@gmail.com>
2019-08-13 13:30:30 +0900

    swiperTemplate

---
+++ components/SwiperTemplate.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+
+export default {
+  head: {
+    link: [
+      {
+        rel: "stylesheet",
+        src: "/css/swiper.min.css"
+      }
+    ]
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    new Swiper(".swiper-container", {});
+  }
+};
+</script>
+
+<style >
+.swiper-container {
+  width: 100%;
+  height: 100%;
+}
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #f00;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,24 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxtjs-demo
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <swiper-template>
+      <div class="swiper-slide">slide1</div>
+      <div class="swiper-slide">slide2</div>
+      <div class="swiper-slide">slide3</div>
+      <div class="swiper-slide">slide4</div>
+    </swiper-template>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import SwiperTemplate from "~/components/SwiperTemplate.vue";
 
 export default {
   components: {
-    AppLogo
+    SwiperTemplate
   }
-}
+};
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
 
