fada3b778ac999b50aa1dde4a25f26c7cb489d13
wddo <wddoddo@gmail.com>
2019-08-14 18:32:38 +0900

    want change event

--- components/SwiperTemplate.vue
+++ components/SwiperTemplate.vue
@@ -1,28 +1,89 @@
 <template>
-  <div class="swiper-container">
+  <div class="swiper-container" :attrs="attrs">
     <div class="swiper-wrapper">
       <slot />
     </div>
+    <div class="swiper-pagination"></div>
   </div>
 </template>
 
 <script>
 import Swiper from "swiper";
+import _ from "lodash";
+
+const getChildrenTagContent = children => {
+  return children.filter(function(node) {
+    return node.tag;
+  });
+};
 
 export default {
-  head: {
-    link: [
-      {
-        rel: "stylesheet",
-        src: "/css/swiper.min.css"
-      }
-    ]
+  head() {
+    return {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "/css/swiper.min.css"
+        }
+      ]
+    };
   },
   data() {
-    return {};
+    return {
+      defaults: {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets"
+        },
+        preloadImages: false,
+        watchSlidesVisibility: true,
+        lazy: {
+          loadPrevNext: true
+        },
+        autoplay: {
+          delay: 3500,
+          disableOnInteraction: false
+        }
+      },
+      freeDefaults: {
+        slidesPerView: "auto",
+        spaceBetween: 0,
+        freeMode: true,
+        roundLengths: true,
+        autoplay: false
+      }
+    };
+  },
+  props: {
+    options: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  computed: {
+    attrs() {
+      return this.$attrs;
+    }
   },
   mounted() {
-    new Swiper(".swiper-container", {});
+    let opts = !this.options.freeMode
+      ? { ...this.defaults }
+      : _.extend({}, this.defaults, this.freeDefaults);
+
+    _.set(
+      opts,
+      "loop",
+      getChildrenTagContent(this.$slots.default).length > 1 && !opts.freeMode
+    );
+
+    opts = _.defaultsDeep(this.options, opts);
+
+    if (!_.isNaN(opts.slidesPerView) && _.isUndefined(opts.loopedSlides))
+      _.set(opts, "loopedSlides", _.ceil(opts.slidesPerView));
+
+    console.log(opts);
+
+    new Swiper(this.$el, opts);
   }
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
   <section class="container">
-    <swiper-template>
+    <swiper-template :options="{autoplay:false, slidesPerView:1.1, spaceBetween: 10}">
+      <div class="swiper-slide">slide1</div>
+      <div class="swiper-slide">slide2</div>
+      <div class="swiper-slide">slide3</div>
+      <div class="swiper-slide">slide4</div>
+    </swiper-template>
+    <br />
+    <swiper-template
+      :options="{autoplay:false, slidesPerView:1.1, spaceBetween: 10, pagination: {type: 'fraction'}}"
+    >
+      <div class="swiper-slide">slide1</div>
+      <div class="swiper-slide">slide2</div>
+      <div class="swiper-slide">slide3</div>
+      <div class="swiper-slide">slide4</div>
+    </swiper-template>
+    <br />
+    <swiper-template :options="{freeMode: true}">
       <div class="swiper-slide">slide1</div>
       <div class="swiper-slide">slide2</div>
       <div class="swiper-slide">slide3</div>
@@ -20,5 +36,8 @@ export default {
 </script>
 
 <style>
+.swiper-slide {
+  height: 150px;
+}
 </style>
 
