eaa310df06fa5b78bb6f5886feab2cf7a20830b4
wddo <wddoddo@gmail.com>
2020-02-04 15:16:42 +0900

    computed problem

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,82 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="wrap">
+    <div :class="this.htmlClass">
+      <!--mounted 에선 첫로드때 바뀜, 이후 updated 에서 바뀜-->
+      <nuxt />
+    </div>
   </div>
 </template>
 
+<script>
+function aaa() {
+  return true
+}
+
+export default {
+  data() {
+    return {
+      is: false
+    }
+  },
+  head() {
+    return {
+      bodyAttrs: {
+        class: this.htmlClass //created, mounted 에선 첫로드때 안바뀜, 이후 updated 에서 바뀜
+      }
+    }
+  },
+  created() {
+    console.log("!!!!!  layout: created")
+
+    //this.changeIs() //호출 시 mounted()에 의한 wrap 아래 클래스도 반영 안됨
+  },
+  mounted() {
+    console.log("!!!!! layout: mounted")
+
+    this.changeIs()
+  },
+  updated() {
+    console.log("!!!!! layout: updated")
+  },
+  computed: {
+    isis() {
+      return this.is
+    },
+    htmlClass() {
+      let arr = ["mobile"]
+
+      if (this.isis) arr.push("ribbon")
+
+      console.log(arr)
+
+      return arr.join(" ")
+    }
+  },
+  methods: {
+    checking() {
+      if (process.client) {
+        return true
+      }
+    },
+    changeIs() {
+      const check = this.checking()
+
+      if (check) {
+        this.is = true
+      } else {
+        this.is = false
+      }
+
+      console.log("this.is", this.is)
+    }
+  }
+}
+</script>
+
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
+    sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -16,7 +86,9 @@ html {
   box-sizing: border-box;
 }
 
-*, *:before, *:after {
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
 }
@@ -50,4 +122,3 @@ html {
   background-color: #35495e;
 }
 </style>
-
